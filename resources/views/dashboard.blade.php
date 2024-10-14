63dea1b499220f468af08f764e40dca903c57b46
Farhan-Syarif-H <farhansyarifhidayatulloh@smkwikrama.sch.id>
2024-10-14 23:21:32 +0700

    update code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <div class="py-12 mt-36">
-        <div class=" max-w-xl mx-auto sm:px-6 lg:px-8 px-5 rounded-md">
+        <div class=" max-w-xl mx-auto sm:px-6 lg:px-8 px-5 rounded-md ">
             <div class=" shadow-md sm:rounded-lg">
-                <div class="p-6 " style="color:#D0C05B">
+                <div class="p-6  " style="color:#D0C05B ">
 
                     <span class="text-5xl">
                {{ __("Hello.") }}<br>
